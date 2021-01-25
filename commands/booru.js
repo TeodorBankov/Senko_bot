9f8c03e35c11e5a69561eb8383f02220e1f34b98
TeodorBankov <teodor2003.tp@gmail.com>
2021-01-25 19:41:43 +0200

    little bug fixes, and little improvements of code

--- commands/booru.js
+++ commands/booru.js
@@ -1,26 +1,27 @@
 const Booru = require('booru');
 
-let fetch_url = async function (site, term, limit) {
-    return await Booru.search(site, term, { limit, random: true })
-}
-
 module.exports = {
     async run(bot) {
         bot.on(/^\/booru(@Senko_kitsune_bot)? (.+)$/, async (msg, props) => {
             const args = props.match[2].split(' ');
+            if (!args[2]) args[2] = 1;
+            if (isNaN(args[2]) || args[2] > 10 || args[2] < 1) return msg.reply.text('Invalid number (1-10)');
             let urls = (await fetch_url(args[0], args[1], args[2])).posts.map(p => p.fileUrl);
             for (i of urls) {
                 try {
-                        console.log("/booru command executed: "+i),
-                        msg.reply.photo(i)
+                    console.log("/booru command executed: " + i);
+                    msg.reply.photo(i, { caption: i });
                 } catch (e) { console.error(e) };
-                // if (/\.swf/.test(i) || /\.webm/.test(i) || /\.gif/.test(i)) {
-                //     try {
-                //         msg.reply(i)
-                //     } catch (e) { console.error(e) }
-                // }
             }
-        })
+            // if (/\.swf/.test(i) || /\.webm/.test(i) || /\.gif/.test(i)) {
+            //     try {
+            //         msg.reply(i)
+            //     } catch (e) { console.error(e) }
+            // }
+        });
     }
 };
 
+let fetch_url = async function (site, term, limit) {
+    return await Booru.search(site, term, { limit, random: true })
+}
--- commands/reddit.js
+++ commands/reddit.js
@@ -4,17 +4,25 @@ module.exports = {
     async run(bot) {
         bot.on(/^\/reddit(@Senko_kitsune_bot)? (.+)/, async (msg, props) => {
             const args = props.match[0].split(' ');
-            redditFetch({
-                subreddit: args[1],
-                sort: 'hot',
-                allowNSFW: true,
-                allowCrossPost: true
-            }).then(post => {
-                console.log("/reddit command executed: "+post.url);
-                msg.reply.photo(post.url);
-            }).catch((e) => {
-                msg.reply.text("You probably entered a wrong subreddit");
-            });
+            if (!args[2]) args[2] = 1;
+            if (isNaN(args[2]) || args[2] > 10 || args[2] < 1) return msg.reply.text('Invalid number (1-10)');
+            for (let i = 0; i < args[2]; i++) {
+                setTimeout(() => {
+                    let sorts = ['hot', 'new'];
+                    let rand = Math.floor(Math.random()*sorts.length)
+                    redditFetch({
+                        subreddit: args[1],
+                        sort: sorts[rand],
+                        allowNSFW: true,
+                        allowCrossPost: true
+                    }).then(post => {
+                        console.log("/reddit command executed, "+ "sorted by: " + sorts[rand] + ', ' + post.url);
+                        msg.reply.photo(post.url, { caption: post.url });
+                    }).catch((e) => {
+                        msg.reply.text("You probably entered a wrong subreddit");
+                    });
+                }, 500 * i);
+            }
         })
     }
 };
